4d5c4431eb7c12db9f486d5e44b5adecd6e3f23b
TUẤN <tranvanlamqbdl@gmail.com>
2024-01-03 08:41:28 +0700

    lam up

---
+++ turtle-sport-be/src/main/java/com/example/turtlesportbe/TurtleSportBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.turtlesportbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurtleSportBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurtleSportBeApplication.class, args);
+    }
+
+}
